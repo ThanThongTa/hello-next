76bcbfe331d988a83c416763db9871f20b29c537
Thong Ta <thong.ta@bpc.ag>
2025-04-22 19:55:41 +0200

    add footer with timestamp

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  const renderedOnTime = new Date().toLocaleString();
+  console.log(`rendered on Server: ${renderedOnTime}`)
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>Page rendered on: {renderedOnTime}</footer>
       </body>
     </html>
   );
